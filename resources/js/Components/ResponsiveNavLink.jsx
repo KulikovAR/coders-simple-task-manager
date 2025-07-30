931af96cd1383ede7967a453f0bc7410268bab7a
Nedlin-cell <paistepaiste302@gmail.com>
2025-07-30 16:11:28 +0300

    hover menu burger

--- resources/js/Components/ResponsiveNavLink.jsx
+++ resources/js/Components/ResponsiveNavLink.jsx
@@ -11,8 +11,8 @@ export default function ResponsiveNavLink({
             {...props}
             className={`flex w-full items-start border-l-4 py-2 pe-4 ps-3 ${
                 active
-                    ? 'border-indigo-400 bg-indigo-50 text-indigo-700 focus:border-indigo-700 focus:bg-indigo-100 focus:text-indigo-800'
-                    : 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800'
+                    ? 'border-accent-blue bg-accent-blue/10 text-accent-blue focus:border-accent-blue focus:bg-accent-blue/20 focus:text-accent-blue'
+                    : 'border-transparent text-text-primary hover:border-border-color hover:bg-secondary-bg hover:text-text-primary focus:border-border-color focus:bg-secondary-bg focus:text-text-primary'
             } text-base font-medium transition duration-150 ease-in-out focus:outline-none ${className}`}
         >
             {children}
