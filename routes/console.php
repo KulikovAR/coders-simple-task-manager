39627feee09b715a9919534e48720b7aa4832275
KulikovAR <106818844+SashkaBackEnd@users.noreply.github.com>
2025-10-06 18:48:45 +0300

    telescope delete

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Настройка расписания команд
+Schedule::command('telescope:cleanup --days=7 --batch-size=1000')
+    ->daily()
+    ->at('02:00')
+    ->name('telescope-cleanup')
+    ->withoutOverlapping()
+    ->runInBackground();
+
+// Очистка файлов (уже настроена в проекте)
+Schedule::command('files:cleanup')
+    ->daily()
+    ->at('02:30')
+    ->name('files-cleanup')
+    ->withoutOverlapping()
+    ->runInBackground();
